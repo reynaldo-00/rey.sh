e8056a7f95095087b56f0eb91a3abe2ec805c646
Chris Cedeno <chrisrey95@gmail.com>
2019-04-05 15:22:58 -0400

    added tracking method

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueAnalytics from 'vue-analytics'
+Vue.use(VueAnalytics, {
+  id: ['UA-126589970-2', 'UA-126589970-3'],
+})
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
 
 
-Vue.use(VueAnalytics, {
-  id: ['UA-126589970-2', 'UA-126589970-3'],
-})
