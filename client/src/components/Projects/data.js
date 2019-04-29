1ad5eca8f571302aa51760cbf8c50158a262172b
Chris Cedeno <chrisrey95@gmail.com>
2019-04-29 19:05:56 -0400

    new tech images + projects order update

--- client/src/components/Projects/data.js
+++ client/src/components/Projects/data.js
@@ -1,34 +1,34 @@
 export default [
     {
-        title: "Flexlog",
-        description: "Fitness tracking app that lets users schedule their workouts and track their progress",
-        techStack: ['react', 'node', 'knex', 'styled', 'OAuth'],
-        cover: "https://res.cloudinary.com/ddpztixxl/image/upload/v1550555909/flexlog.png",
-        repoLink: "https://github.com/Lambda-School-Labs/labs9-workout-tracker",
-        deployLink: "https://flexlog.app/"
-    },
-    {
-        title: "Google Keep Clone",
-        description: "Clone of Google's note taking app, Google Keep",
-        techStack: ['react', 'node', 'knex', 'styled', 'jwt'],
-        cover: "https://res.cloudinary.com/ddpztixxl/image/upload/v1550555801/google-keep.png",
-        repoLink: "https://github.com/reynld/keep-clone",
-        deployLink: "https://google-keep-clone.now.sh/"
-    },
-    {
         title: "Carbtographer",
         description: "Map that shows macro friendly options for nearby restaurants",
-        techStack: ['react', 'node', 'graphql', 'golang', 'styled', 'react-map-gl',],
+        techStack: ['react', 'redux', 'react-map-gl', 'sass', 'styled', 'golang', 'postgres', 'redis', 'aws'],
         cover: "https://res.cloudinary.com/ddpztixxl/image/upload/v1554436775/carbto-min.png",
         repoLink: "https://github.com/reynld/carbtographer",
         deployLink: "https://www.carbtographer.com/"
     },
     {
+        title: "Flexlog",
+        description: "Fitness tracking app that lets users schedule their workouts and track their progress",
+        techStack: ['react', 'node', 'knex', 'styled', 'firebase', 'postgres', 'heroku', 'netlify'],
+        cover: "https://res.cloudinary.com/ddpztixxl/image/upload/v1550555909/flexlog.png",
+        repoLink: "https://github.com/Lambda-School-Labs/labs9-workout-tracker",
+        deployLink: "https://flexlog.app/"
+    },
+    {
         title: "Portfolio",
         description: "Personal portfolio site built with Vue and Golang",
-        techStack: ['vue', 'golang', 'graphql', 'sass', 'OAuth'],
+        techStack: ['vue', 'sass', 'styled', 'golang', 'zeit'],
         cover: "https://res.cloudinary.com/ddpztixxl/image/upload/v1550555424/portfolio.png",
         repoLink: "https://github.com/reynld/rey.sh",
         deployLink: "https://www.rey.sh/"
+    },
+    {
+        title: "Google Keep Clone",
+        description: "Clone of Google's note taking app, Google Keep",
+        techStack: ['react', 'node', 'knex', 'styled'],
+        cover: "https://res.cloudinary.com/ddpztixxl/image/upload/v1550555801/google-keep.png",
+        repoLink: "https://github.com/reynld/keep-clone",
+        deployLink: "https://google-keep-clone.now.sh/"
     },
 ]
